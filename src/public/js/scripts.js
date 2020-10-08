b970f816b82d13272a4d59b1780e115bc7456e85
damenor <damenor.develop@gmail.com>
2020-10-08 07:59:00 +0200

    fixed drag and drop

--- src/public/js/scripts.js
+++ src/public/js/scripts.js
@@ -1,65 +1,24 @@
 const container = document.getElementById('container')
 
 function handleDrop (event) {
-
-  console.log('File(s) dropped', event.dataTransfer.items)
   event.preventDefault()
-
-  if (event.dataTransfer.items) {
-    // Use DataTransferItemList interface to access the file(s)
-    for (var i = 0; i < event.dataTransfer.items.length; i++) {
-      // If dropped items aren't files, reject them
-      if (event.dataTransfer.items[i].kind === 'file') {
-        var file = event.dataTransfer.items[i].getAsFile();
-        console.log('... file[' + i + '].name = ' + file.name);
-      }
-    }
-  } else {
-    // Use DataTransfer interface to access the file(s)
-    for (var i = 0; i < event.dataTransfer.files.length; i++) {
-      // console.log('... file[' + i + '].name = ' + event.dataTransfer.files[i].name);
-      console.log('... file[' + i + '].name = ' + event.target.files[i].name);
-    }
-  } 
-  
-  // Pass event to removeDragData for cleanup
+  const file =  event.dataTransfer.files[0]
+  fetchImage(file)
   removeDragData(event)
-
 }
 
 function handleDragOver(event) {
-  console.log('File(s) in drop zone')
   event.preventDefault()
 }
 
 function removeDragData(event) {
-  console.log('Removing drag data')
-
-  if (event.dataTransfer.items) {
-    // Use DataTransferItemList interface to remove the drag data
-    event.dataTransfer.items.clear();
-  } else {
-    // Use DataTransfer interface to remove the drag data
-    event.dataTransfer.clearData();
-  }
+  event.dataTransfer.items ? event.dataTransfer.items.clear() : event.dataTransfer.clearData()
 }
 
 function handleChangeImage(event) {
   handleChangeElement (buildLoading)
   const file = event.target.files[0]
-  const formData = new FormData()
-  formData.append('image', file)
-  fetch('/upload', {
-    method: 'POST',
-    body: formData
-  })
-  .then(res => res.json())
-  .then(data => {
-    console.log('response', data)
-    handleChangeElement(() => {
-      return buildImageUploaded(data.nameFile)
-    })
-  })
+  fetchImage(file)
 }
 
 function handleChangeElement (builder) {
@@ -84,22 +43,30 @@ function buildImageUploaded (nameImage) {
       <img id="image-uploaded" src="${urlImage}" alt="image uploaded">
     </div>
 
-
     <div id="path-image">
       <input 
         id="path-image-input"
         type="text" 
         disabled
         value="${urlImage}">
-      <button onclick="copyToClipboard('${urlImage}')">Copy link</button>
+      <button onclick="copyToClipboard()">Copy link</button>
     </div>
   `
 }
 
-function copyToClipboard(path) {
+function copyToClipboard() {
   const pathImageInput = document.getElementById('path-image-input')
-  console.log(path, pathImageInput)
   pathImageInput.select()
-  pathImageInput.setSelectionRange(0, 99999)
   document.execCommand('copy')
+}
+
+function fetchImage (file) {
+  const formData = new FormData()
+  formData.append('image', file)
+  fetch('/upload', {
+    method: 'POST',
+    body: formData, 
+  })
+  .then(res => res.json())
+  .then(data => handleChangeElement(() => buildImageUploaded(data.nameFile)))
 }
